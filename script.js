4e3c24ce25c7f2c01a396cd1f2d29fe872bab460
Pascal P <67502663+PascalP1998@users.noreply.github.com>
2021-05-03 19:07:38 +0200

    Added second generator

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
-let slider = document.getElementById("myRange");
-let output = document.getElementById("value");
-output.innerHTML = slider.value; 
+let slider_1 = document.getElementById("myRange_1");
+let output_1 = document.getElementById("value_1");
+output_1.innerHTML = slider_1.value; 
 
-slider.oninput = function() {
-    output.innerHTML = this.value;
-  } 
+slider_1.oninput = function() {
+    output_1.innerHTML = this.value;
+    }
 
-const syllabes = Array("ka", "ki", "ku", "ke", "ku", "ko", "ta", "ti", "tu", "te", "to", "sa", "si", "su", "se", "so", "na", "ni", "nu", "ne", "no", "ha", "hi", "fu", "he", "ho", "ma", "mi", "mu", "me", "mo", "ya", "yu", "yo", "ra", "ri", "ru", "re", "ro", "wa", "wo");
-const end = Array("", "m", "n", "a", "e", "o");
+let slider_2 = document.getElementById("myRange_2");
+let output_2 = document.getElementById("value_2");
+output_2.innerHTML = slider_2.value; 
 
+slider_2.oninput = function() {
+    output_2.innerHTML = this.value;
+    } 
 
-function createName(array1, array2, number) {
-    let field = document.getElementById("name-field")
+const jap_syllabes = Array("a", "i", "u", "e", "o", "n", "ka", "ki", "ke", "ku", "ko", "ta", "chi", "tsu", "te", "to", "sa", "shi", "su", "se", "so", "na", "ni", "nu", "ne", "no", "ha", "hi", "fu", "he", "ho", "ma", "mi", "mu", "me", "mo", "ya", "yu", "yo", "ra", "ri", "ru", "re", "ro", "wa", "ga", "gi", "gu", "ge", "go", "za", "ji", "zu", "ze", "zo", "da", "de", "do", "ba", "bi", "bu", "be", "bo", "pa", "pi", "pu", "pe", "po");
+
+const vowels = Array("a", "e", "i", "o", "u", "y");
+const consonants = Array("q", "w", "r", "t", "z", "s", "p", "d", "f", "g", "h", "j", "k", "l", "m", "n", "x", "c", "v", "b", "n", "m");
+
+function createJapName(array, number) {
+    let field = document.getElementById("name-field-1");
     let name = "";
     for (let i=1; i <= number; i++) {
-        const randomElement = array1[Math.floor(Math.random() * array1.length)];
-        name += randomElement
+        const randomElement = array[Math.floor(Math.random() * array.length)];
+        name += randomElement;
     }
-    const randomElement = array2[Math.floor(Math.random() * array2.length)];
-    name += randomElement;
-    console.log(name);
-    field.innerHTML = name;
+    if (name.match(/([a-z])\1+/)) {
+        while (true) {
+            const randomElement = array[Math.floor(Math.random() * array.length)];
+            name.replace(/([a-z])\1+/, randomElement);
+            break;
+        }
+    } else {
+        console.log(name);
+        field.innerHTML = name;
+    }
+}
+
+function createName(array1, array2, number) {
+    let field = document.getElementById("name-field-2");
+    let name = "";
+    for (let i=1; i <= number; i++) {
+        const randomElement1 = array1[Math.floor(Math.random() * array1.length)];
+        const randomElement2 = array2[Math.floor(Math.random() * array2.length)];
+        name += randomElement1 + randomElement2;
+    }
+    if (name.match(/([a-z])\1+/)) {
+        while (true) {
+            const randomElement1 = array1[Math.floor(Math.random() * array1.length)];
+            const randomElement2 = array2[Math.floor(Math.random() * array2.length)];
+            name.replace(/([a-z])\1+/, randomElement1 + randomElement2);
+            break;
+        }
+    } else {
+        console.log(name);
+        field.innerHTML = name;
+    }
 }
 
